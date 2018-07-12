417dbdaaf440e0182f25c25b794b8c4746c118f4
Pedrogrc <pedroca.calxit@gmail.com>
2018-07-11 21:29:11 -0300

    First Commit

--- creed/urls.py
+++ creed/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lista', views.lista_clientes, name='lista_clientes'),
-    path('novo', views.cadastro_cliente, name='cadastro_cliente'),
+    path('/lista', views.lista_clientes, name='lista_clientes'),
+    path('/novo', views.cadastro_cliente, name='cadastro_cliente'),
     path('atualizar/<int:pk>/', views.editar_cliente, name='editar_cliente'),
     path('delete/<int:pk>/', views.delete_cliente, name='delete_cliente'),
 ]
